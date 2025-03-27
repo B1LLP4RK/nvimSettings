a58b4042e9d234fe3fb826127b9833d5db1fb54c
Park Junha <bd08001@gmail.com>
2025-03-27 17:31:31 +0800

    Initial commit for init.lua
    Planning to migrate to init.lua

---
+++ init.lua
@@ -0,0 +1,14 @@
+vim.o.gdefault = true
+vim.o.splitright = true
+vim.o.splitbelow = true
+vim.o.expandtab = true
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.incsearch = true
+vim.o.tabstop=4
+vim.o.shiftwidth=4
+vim.opt.diffopt:append('vertical')
+vim.opt.shortmess:append('c')
+vim.o.mouse = 'a'
